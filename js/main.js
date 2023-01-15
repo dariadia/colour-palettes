3d088004729efab4bee0b0cd5e268ff56095e902
Daria <daria.diachkova@mail.ru>
2023-01-15 15:42:29 +0100

    feat(app): add colour converter

---
+++ js/main.js
@@ -0,0 +1,42 @@
+import { Color } from "color"
+
+const getHslColour = (colour) => Color(Color(colour).hslString())
+
+export const getPrimaryColour = (colour) => [ getHslColour(colour).rgb() ]
+
+export const getComplementaryColour = (colour) => [
+  getHslColour(colour).rgb(),
+  getHslColour(colour).rotate(180).rgb()
+]
+
+export const getTriadColour = (colour) => [
+  getHslColour(colour).rgb(),
+  getHslColour(colour).rotate(120).rgb(),
+  getHslColour(colour).rotate(120).rgb()
+]
+
+export const getAnalogousColour = (colour) => [
+  getHslColour(colour).rotate(-30).rgb(),
+  getHslColour(colour).rgb(),
+  getHslColour(colour).rotate(30).rgb()
+]
+
+export const getSplitComplementaryColour = (colour) => [
+  getHslColour(colour).rgb(),
+  getHslColour(colour).rotate(-30).rgb(),
+  getHslColour(colour).rotate(30).rgb()
+]
+
+export const getTetradicColour = (colour) => [
+  getHslColour(colour).rgb(),
+  getHslColour(colour).rotate(90).rgb(),
+  getHslColour(colour).rotate(180).rgb(),
+  getHslColour(colour).rotate(240).rgb()
+]
+
+export const getSquareColour = (colour) => [
+  getHslColour(colour).rgb(),
+  getHslColour(colour).rotate(90).rgb(),
+  getHslColour(colour).rotate(180).rgb(),
+  getHslColour(colour).rotate(270).rgb()
+]
